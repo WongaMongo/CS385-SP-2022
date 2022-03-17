2eafa600d6422b97b7ba05bedc98b65035b02a49
WongaMongo <82738244+WongaMongo@users.noreply.github.com>
2022-03-16 18:56:07 -0800

    Added Items from Lecture 11
    A bit confused now. As a zoom watcher there was slightly hard to hear chatter about orbits. Unsure if both the Earth AND the Moon are orbiting the sun. OR if the Earth is orbiting the Sun, and then the Moon orbits the Earth. 
    
    Noticed that strict mode is on. Gives a warning since we've done item = item.new without using var/let, but I'm not sure if it affects the actual implementation or not. Just gonna leave it there.

--- Assignment 4/main.js
+++ Assignment 4/main.js
@@ -11,7 +11,13 @@ function init() {
     gl.enable(gl.DEPTH_TEST);
    
     // Add your sphere creation and configuration code here
-    // sun = new Sphere()
+    Sun = new Sphere();
+    Earth = new Sphere();
+    Moon = new Sphere();
+
+    Sun.radius = 10
+    Earth.radius = 5
+    Moon.radius = 2
 
     requestAnimationFrame(render);
 }
@@ -19,10 +25,39 @@ function init() {
 function render() {
 
     // Update your motion variables here
+    ms = new MatrixStack();
+    var V = translate(0.0, 0.0, -0.5 * (near + far))
+    ms.load(V)
+
+    ms.push();
+    ms.scale(Sun.radius);
+    Sun.MV = ms.current();
+    Sun.render();
+    ms.pop();
+
+    ms.push();
+    ms.rotate(year, axis);
+    ms.translate(distance, 0, 0);
+    ms.push();
+    // Possibly include this above push?
+    // Hard to tell since he specifies 
+    // The earth orbits the sun, the moon orbits the sun
+    // Did he mean the earht orbits the sun, the moon orbits the earth?
+    ms.rotate(day, axis);
+    ms.scale(Earth.radius);
+    Earth.MV = ms.current();
+    Earth.render();
+    ms.pop;
+    ms.translate(Moon.distance, 0, 0);
+    ms.scale(Moon.radius);
+    Moon.MV = ms.current();
+    Moon.render();
+    ms.pop;
 
     gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
     
     // Add your rendering sequence here
+    sun.render()
 
     requestAnimationFrame(render);
 }
