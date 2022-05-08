757ec39ea6ea72420308faf179aa2effe3ba128f
WongaMongo <82738244+WongaMongo@users.noreply.github.com>
2022-05-07 22:15:28 -0700

    Fixed image view, testing zoom
    I live in the documentation tabs of the internet. Fixing images I managed to get the image to fit in the canvas, although currently I need to think of how I want to display larger images of different proportions. My current canvas has a ratio of 16:10 so while images small and large will be scaled accordingly, if they aren't of a 16:10 ratio they'll warp. Also using the click  button to check out zooming functions.
    
    New goal is to have zooming, panning and rotation as sliders. If I don't take too long I'd like to have have click-drag panning and mouse wheel zooming as well, just because they would feel a lot more organic. Of course I'm aiming for minimum viable goals as the semester is winding down.

--- Image Viewer/imageViewer.js
+++ Image Viewer/imageViewer.js
@@ -3,23 +3,39 @@
 const img = new Image();
 img.src = './wicked.jpg';
 
-console.log("Yo")
+console.log("Yo");
 
-    
+console.log(`Image Resolution  -> [H: ${img.height} | W: ${img.width}]`);
+
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-// context.fillStyle = "#000000";
-// context.fillRect(0, 0, 0, 0)
-img.onload = () =>{
-    console.log("here")
-    context.drawImage(img, 0, 0, 960, 600, 0, 0, 960, 600);
-}
 
+console.log(`Canvas Resolution -> [H: ${canvas.height} | W: ${canvas.width}]`);
+
+var hRatio = canvas.height / img.height;
+var wRatio = canvas.width / img.width;
+var tRatio = Math.min(hRatio, wRatio)
+
+context.fillStyle = "#CAEAE9";
+context.fillRect(0, 0, canvas.width, canvas.height)
+img.onload = () =>{
+    console.log("here");
+    context.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+}
+// context.scale(2, 2)
 
 console.log("Ye")
 
 var clicks = 0;
 function countClicks(){
     clicks+= 1;
-    document.getElementById("click").innerHTML = clicks
+    // img.width += 100
+    // canvas.width += 100
+    let a = 0
+    let b = 0
+    if(clicks % 2 === 0){ a = b = 0.5 }
+    else{ a = b = 2 }
+    context.scale(a, b)
+    img.src = './wicked.jpg'
+    document.getElementById("click").innerHTML = clicks;
 }
