1b384e3d41614c86e692075ede8ce57f90cf371e
WongaMongo <82738244+WongaMongo@users.noreply.github.com>
2022-05-13 14:46:47 -0700

    Added in sliders
    With the exam done and hopefully not a complete misery, I'm back on the project. The sliders were surprisingly easy to add in despite how I imagined it. After consulting the documentation sliders are literally an html object type. Using input and event listeners I can now move values from the html to the js file and reflect the change in the html. Which I thought was going to be a big headache but ended up moving along very smoothly, I thank google everyday.

--- Image Viewer/imageViewer.js
+++ Image Viewer/imageViewer.js
@@ -1,5 +1,8 @@
 
 
+// Draw image only allows data in certain forms. 
+// A normal image is not a that form
+// This allows an image to be a HTMLImageElement
 const img = new Image();
 img.src = './wicked.jpg';
 
@@ -7,24 +10,61 @@ console.log("Yo");
 
 console.log(`Image Resolution  -> [H: ${img.height} | W: ${img.width}]`);
 
+// Getting canvas proportions from the html
 const canvas = document.getElementById("canvas");
+
+// Declaring it as a 2d object
 const context = canvas.getContext("2d");
 
 console.log(`Canvas Resolution -> [H: ${canvas.height} | W: ${canvas.width}]`);
 
+// Scaling which I couldn't get right and needed to look up
+// Apparently this is the scaling norm for canvas and images on html
+// I'm not sure why it uses the lesser of height and width, 
+// instead of height for height, width for width, but if it works. . .
 var hRatio = canvas.height / img.height;
 var wRatio = canvas.width / img.width;
 var tRatio = Math.min(hRatio, wRatio)
 
-context.fillStyle = "#CAEAE9";
+// Just fills the canvas background
+context.fillStyle = "#CCECEC";
 context.fillRect(0, 0, canvas.width, canvas.height)
-img.onload = () =>{
+
+// After the image loads then I draw it into the canvas
+img.onload = () => {
     console.log("here");
     context.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
 }
 // context.scale(2, 2)
 
 console.log("Ye")
+
+// Getting slider values from the html
+var panValue = document.getElementById("pan");
+var zoomValue = document.getElementById("zoom");
+var rotateValue = document.getElementById("rotate");
+
+// Getting output values from the html
+var curPan = document.getElementById("curPan");
+var curZoom = document.getElementById("curZoom");
+var curRotate = document.getElementById("curRotate");
+
+// Updates the numbers by the sliders
+updatePan = () => { curPan.innerHTML = panValue.value; }
+updateZoom = () => { curZoom.innerHTML = zoomValue.value; }
+updateRotate = () => { curRotate.innerHTML = rotateValue.value; }
+
+// Event listeners, changes slider value when user changes value
+panValue.addEventListener('input', updatePan); updatePan();
+zoomValue.addEventListener('input', updateZoom); updateZoom();
+rotateValue.addEventListener('input', updateRotate); updateRotate();
+
+
+console.log("Doc")
+
+// Button clicking function
+// Just allows me to test methods and transformations
+// Before I catually code them in.
 
 var clicks = 0;
 function countClicks(){
